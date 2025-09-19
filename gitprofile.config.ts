828e6d4cc7e189c45664ccd04c2c317a273a26ec
iontail <leechanhye3722@gmail.com>
2025-09-19 14:40:53 +0900

    update

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -164,7 +164,7 @@ const CONFIG = {
   // Track visitor interaction and behavior. https://www.hotjar.com
   hotjar: { id: '', snippetVersion: 6 },
   themeConfig: {
-    defaultTheme: 'lofi',
+    defaultTheme: 'winter',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
